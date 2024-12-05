811aae2380bf489ddcd8c6b1e89d6dcfc62eb746
ikerkor <ikorker@tutanota.com>
2024-12-05 19:03:33 +0100

    lanik ez dagoenen adierazi eta lan laguntzaileak ez zerrendatu.

--- alarma_kudeaketa.py
+++ alarma_kudeaketa.py
@@ -13,14 +13,19 @@ async def kudeatu(update: Update, context: CallbackContext) -> int:
 
     jobs = context.job_queue.jobs()
     mezua = ""
-    for i, job in enumerate(jobs):
-        if str(update.message.chat.id) in job.name:
-            mezua += f"{i}.\U0001F514 {job.name}\n"
-    try:
-        mezua = mezua[0:-2]
-    except:
-        pass
-    await update.message.reply_text(mezua)
+    kontagailu = 1
+    for job in jobs:
+        if str(update.message.chat.id) in job.name and "_aux" not in job.name:
+            mezua += f"{kontagailu}.\U0001F514 {job.name}\n"
+            kontagailu += 1
+    if mezua.len() != "":
+        try:
+            mezua = mezua[0:-2]
+            await update.message.reply_text(mezua)
+        except:
+            pass
+    else:
+        await update.message.reply_text("Ez duk alarmarik gordeta, txikito.")
     return ZERRENDA
 
 async def zerrenda(update: Update, context: CallbackContext) -> int:
