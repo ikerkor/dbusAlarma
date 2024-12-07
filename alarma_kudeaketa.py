f970ecbdd78359aa74f93a8a67da774fab7b524c
ikerkor <ikorker@tutanota.com>
2024-12-07 20:34:06 +0100

    Kendu elkarrizketa-egoera bypasseatu.

--- alarma_kudeaketa.py
+++ alarma_kudeaketa.py
@@ -16,17 +16,19 @@ async def kudeatu(update: Update, context: CallbackContext) -> int:
     kontagailu = 1
     for job in jobs:
         if str(update.message.chat.id) in job.name and "_aux" not in job.name:
-            mezua += f"{kontagailu}.\U0001F514 {job.name}\n"
+                mezua += f"{kontagailu}.\U0001F514 {job.name}\n"
             kontagailu += 1
     if mezua != "":
         try:
             mezua = mezua[0:-2]
             await update.message.reply_text(mezua)
+
         except:
             pass
     else:
         await update.message.reply_text("Ez duk alarmarik gordeta, txikito.")
-    return ZERRENDA
+        await update.message.reply_text('Alarmarik kendu nahi baduzu, idatzi beraren zenbakia. Bestela, 0 edo /utzi.')
+    return KENDU
 
 async def zerrenda(update: Update, context: CallbackContext) -> int:
     await update.message.reply_text('Alarmarik kendu nahi baduzu, idatzi beraren zenbakia. Bestela, 0 edo /utzi.')
